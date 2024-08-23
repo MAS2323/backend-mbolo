cda95c536edbe7e643ef873fc5153f7282953e8e
Ernesto Vicencio Mas <masoneweernesto@gmail.com>
2024-08-23 16:02:14 +0100

    tres commit

--- controllers/subcategoriesController.js
+++ controllers/subcategoriesController.js
@@ -1,4 +1,4 @@
-const Subcategory = require("../models/subcategory");
+const Subcategory = require("../models/Subcategory");
 const mongoose = require("mongoose");
 const multer = require("multer");
 const path = require("path");
